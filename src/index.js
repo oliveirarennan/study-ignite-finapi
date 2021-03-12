43a4344ca80ba7f84c5ef4a75358ad9b6e44ad95
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-03-12 12:43:13 -0300

    Deposit Operation

--- src/index.js
+++ src/index.js
@@ -51,4 +51,21 @@ app.get("/statement", verifyIfExistsAccountCpf, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCpf, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
 app.listen(3333);
