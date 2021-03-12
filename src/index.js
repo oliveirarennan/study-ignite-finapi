dbfabdb4efaacf117083b38f81e8aeb730d3cb9c
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-03-12 11:50:40 -0300

    Create account

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists!",
+    });
+  }
+
+  customers.push({
+    id: uuidv4(),
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
